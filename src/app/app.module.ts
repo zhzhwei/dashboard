522219286989a6481728897313d9f2d1a1167692
zhzhwei <lares2333@gmail.com>
2023-10-19 19:45:59 +0200

    add barchart example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { GridStackModule } from './gridstack/gridstack.module';
+import { BarChartComponent } from './barchart/barchart.component';
 
 @NgModule({
   imports: [BrowserModule, GridStackModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, BarChartComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
