0cd43eb2747682be87c9adfb117d32021b0eac43
Alienmiep <alienmiep@gmx.de>
2024-01-17 19:19:25 +0100

    Cleanup, add test button for data queries

--- src/app/dialog/vis-gen-dialog/bar-chart-preview/bar-chart-preview.component.ts
+++ src/app/dialog/vis-gen-dialog/bar-chart-preview/bar-chart-preview.component.ts
@@ -1,25 +1,61 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+
+import { RdfDataService } from "../../../services/rdf-data.service";
 
 @Component({
-  selector: 'app-bar-chart-preview',
-  templateUrl: './bar-chart-preview.component.html',
-  styleUrls: ['./bar-chart-preview.component.css']
+    selector: "app-bar-chart-preview",
+    templateUrl: "./bar-chart-preview.component.html",
+    styleUrls: ["./bar-chart-preview.component.css"],
 })
 export class BarChartPreviewComponent implements OnInit {
-
     @Input() queryParameters: Object;
     @Input() selectProperties: string[];
 
+    public xProperty;
 
-  constructor() {
-   }
-    ngOnInit(): void {
+    public queryMappings: { [key: string]: string } = {
+        jobName: `FILTER contains(?title, "$value$").`,
+        createdBefore: `?s edm:dateCreated ?created. FILTER (xsd:dateTime("$value$T00:00:00Z") > xsd:dateTime(?created)).`,
+        createdAfter: `?s edm:dateCreated ?created. FILTER (xsd:dateTime(?created) > xsd:dateTime("$value$T00:00:00Z")).`,
+        fulltimeJob: `?s mp:isFulltimeJob "$value$"^^xsd:boolean.`,
+        limitedJob: `?s mp:isLimitedJob "$value$"^^xsd:boolean.`,
+        skill: `?s edm:hasSkill ?skill. ?skill edm:textField "$value$"@de`,
+    };
+
+    constructor(private rdfDataService: RdfDataService) {}
+    ngOnInit(): void {}
+
+    logQueryParameters() {
+        console.log("Query Parameters:", this.queryParameters);
+        console.log("select properties (probably the more important ones):", this.selectProperties);
+    }
+    updateXProperty() {
+        // Access the selected value using document.getElementById
+        const xPropertyElement = document.getElementById("x_property");
+        if (xPropertyElement instanceof HTMLSelectElement) {
+            // Ensure the element is a select element
+            this.xProperty = xPropertyElement.value;
+        }
     }
 
-  logQueryParameters() {
-    console.log('Query Parameters:', this.queryParameters);
-    console.log('select properties (probably the more important ones):', this.selectProperties);
-    
-  }
+    generateQuery() {
+        this.updateXProperty();
 
+        let query =
+            this.rdfDataService.prefixes +
+            `SELECT ?${this.xProperty} (COUNT(DISTINCT ?s) AS ?occurrences) WHERE {
+            ?s rdf:type edm:JobPosting.
+            ?s edm:title ?title.`;
+
+        // Add statements based on queryParameters
+        for (const key in this.queryParameters) {
+            if (this.queryParameters.hasOwnProperty(key) && this.queryMappings[key]) {
+                const filterString = this.queryMappings[key].replace("$value$", this.queryParameters[key]);
+                query += filterString;
+            }
+        }
+        console.log(query);
+
+        return query + "}";
+    }
 }
--- src/app/dialog/vis-gen-dialog/vis-gen-dialog.component.ts
+++ src/app/dialog/vis-gen-dialog/vis-gen-dialog.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit, ViewChildren, QueryList, ElementRef, ComponentFactoryResolver, Injector } from '@angular/core';
-import { BarChartPreviewComponent } from './bar-chart-preview/bar-chart-preview.component';
-import { LineChartPreviewComponent } from './line-chart-preview/line-chart-preview.component';
-import { HttpClient } from '@angular/common/http';
-import { GridStackComponent } from '../../gridstack/gridstack.component';
+import { Component, OnInit, ViewChildren, QueryList, ElementRef, ComponentFactoryResolver, Injector } from "@angular/core";
+import { BarChartPreviewComponent } from "./bar-chart-preview/bar-chart-preview.component";
+import { LineChartPreviewComponent } from "./line-chart-preview/line-chart-preview.component";
+import { HttpClient } from "@angular/common/http";
+import { GridStackComponent } from "../../gridstack/gridstack.component";
 
-import { ChartService } from '../../services/chart.service';
-import { DialogService } from '../../services/dialog.service';
-import { RdfDataService } from '../../services/rdf-data.service';
+import { ChartService } from "../../services/chart.service";
+import { DialogService } from "../../services/dialog.service";
+import { RdfDataService } from "../../services/rdf-data.service";
 
-import * as d3 from 'd3';
-import { SystemService } from 'src/app/services/system.service';
+import * as d3 from "d3";
+import { SystemService } from "src/app/services/system.service";
 
 @Component({
-    selector: 'app-vis-gen-dialog',
-    templateUrl: './vis-gen-dialog.component.html',
-    styleUrls: ['./vis-gen-dialog.component.css']
+    selector: "app-vis-gen-dialog",
+    templateUrl: "./vis-gen-dialog.component.html",
+    styleUrls: ["./vis-gen-dialog.component.css"],
 })
 export class VisGenDialogComponent implements OnInit {
     //important object
@@ -22,10 +22,15 @@ export class VisGenDialogComponent implements OnInit {
     currentPreviewContent: any = undefined;
     currentPreviewContentRef: any = undefined;
 
-    constructor(private chartService: ChartService, private dialogService: DialogService, 
-        private rdfDataService: RdfDataService, private systemService: SystemService, private http: HttpClient, 
-        private componentFactoryResolver: ComponentFactoryResolver, private injector: Injector) { }
-
+    constructor(
+        private chartService: ChartService,
+        private dialogService: DialogService,
+        private rdfDataService: RdfDataService,
+        private systemService: SystemService,
+        private http: HttpClient,
+        private componentFactoryResolver: ComponentFactoryResolver,
+        private injector: Injector
+    ) {}
 
     public chartType: string;
     public gridStack: GridStackComponent;
@@ -35,11 +40,11 @@ export class VisGenDialogComponent implements OnInit {
 
     //checkboxes
     public jobNameCheckbox: boolean = false;
-    public jobNameInput: string = '';
+    public jobNameInput: string = "";
     public createdBeforeCheckbox: boolean = false;
-    public createdBeforeDate: string = '';
+    public createdBeforeDate: string = "";
     public createdAfterCheckbox: boolean = false;
-    public createdAfterDate: string = '';
+    public createdAfterDate: string = "";
     public applicantNumberCheckbox: boolean = false;
     public applicantNumber: number = 1;
     public fulltimeJobCheckbox = false;
@@ -47,14 +52,16 @@ export class VisGenDialogComponent implements OnInit {
     public limitedJobCheckbox = false;
     public limitedJob: boolean = true;
     public listsSkillCheckbox: boolean = false;
-    public skill: string = '';
+    public skill: string = "";
 
     public allSuggestions: string[] = [];
     public selectProperties: string[] = ["s"];
 
-    @ViewChildren('propertyCheckbox') propertyCheckboxes!: QueryList<ElementRef<HTMLInputElement>>;
+    @ViewChildren("propertyCheckbox") propertyCheckboxes!: QueryList<ElementRef<HTMLInputElement>>;
 
-    public barQuery = this.rdfDataService.prefixes + `
+    public barQuery =
+        this.rdfDataService.prefixes +
+        `
         select ?title where { 
             ?s rdf:type edm:JobPosting.
             ?s edm:title ?title.
@@ -63,181 +70,160 @@ export class VisGenDialogComponent implements OnInit {
 
     ngOnInit(): void {
         this.previewResults = [];
-        this.onSearch()
-        this.http.get('assets/job_name_suggestions.txt', { responseType: 'text' }).subscribe(data => {
-        // Split the content into an array of suggestions (assuming one suggestion per line)
-            this.allSuggestions = data.split('\n').map(suggestion => suggestion.trim());
-    });
+        this.onSearch();
+        this.http.get("assets/job_name_suggestions.txt", { responseType: "text" }).subscribe((data) => {
+            // Split the content into an array of suggestions (assuming one suggestion per line)
+            this.allSuggestions = data.split("\n").map((suggestion) => suggestion.trim());
+        });
     }
 
     ngAfterViewInit(): void {
-        this.updateProperties()
+        this.updateProperties();
     }
 
     updateQueryParameters() {
-    if (this.jobNameCheckbox && this.jobNameInput != undefined) {
-        if(this.jobNameInput.trim() !== '') {
-            this.queryParameters['jobName'] = this.jobNameInput.trim();
+        if (this.jobNameCheckbox && this.jobNameInput != undefined) {
+            if (this.jobNameInput.trim() !== "") {
+                this.queryParameters["jobName"] = this.jobNameInput.trim();
+            } else {
+                delete this.queryParameters["jobName"];
+            }
+        } else {
+            // If checkbox is unchecked or input is empty, remove the key from queryParameters
+            delete this.queryParameters["jobName"];
         }
-        else {
-            delete this.queryParameters['jobName'];
+
+        // Update queryParameters for createdBefore
+        if (this.createdBeforeCheckbox && this.createdBeforeDate) {
+            this.queryParameters["createdBefore"] = this.createdBeforeDate;
+        } else {
+            delete this.queryParameters["createdBefore"];
         }
-    } else {
-      // If checkbox is unchecked or input is empty, remove the key from queryParameters
-        delete this.queryParameters['jobName'];
-    }
-    
-    // Update queryParameters for createdBefore
-    if (this.createdBeforeCheckbox && this.createdBeforeDate) {
-        this.queryParameters['createdBefore'] = this.createdBeforeDate;
-    } else {
-        delete this.queryParameters['createdBefore'];
-    }
 
-    // Update queryParameters for createdAfter
-    if (this.createdAfterCheckbox && this.createdAfterDate) {
-        this.queryParameters['createdAfter'] = this.createdAfterDate;
-    } else {
-        delete this.queryParameters['createdAfter'];
-    }
-    
-    // Update queryParameters for applicantNumber
-    if (this.applicantNumberCheckbox && this.applicantNumber !== undefined) {
-        this.queryParameters['applicantNumber'] = this.applicantNumber;
-    } else {
-        delete this.queryParameters['applicantNumber'];
-    }
-    
-    // fulltimeJob and limitedJob
-    if (this.fulltimeJobCheckbox && this.fulltimeJob !== undefined) {
-        this.queryParameters['fulltimeJob'] = JSON.parse(this.fulltimeJob.toString());
-    } else {
-        delete this.queryParameters['fulltimeJob'];
-    }
-
-    if (this.limitedJobCheckbox && this.limitedJob !== undefined) {
-        this.queryParameters['limitedJob'] = JSON.parse(this.limitedJob.toString());
-    } else {
-        delete this.queryParameters['limitedJob'];
-    }
-
-    // Update queryParameters for skill
-    if (this.listsSkillCheckbox  && this.skill != undefined) {
-        if(this.skill.trim() !== '') {
-            this.queryParameters['skill'] = this.skill.trim();
+        // Update queryParameters for createdAfter
+        if (this.createdAfterCheckbox && this.createdAfterDate) {
+            this.queryParameters["createdAfter"] = this.createdAfterDate;
+        } else {
+            delete this.queryParameters["createdAfter"];
         }
-        else {
-            delete this.queryParameters['skill'];
-        }
-    } else {
-        delete this.queryParameters['skill'];
-    }
 
-    console.log('Updated queryParameters:', this.queryParameters);
+        // Update queryParameters for applicantNumber
+        if (this.applicantNumberCheckbox && this.applicantNumber !== undefined) {
+            this.queryParameters["applicantNumber"] = this.applicantNumber;
+        } else {
+            delete this.queryParameters["applicantNumber"];
+        }
+
+        // fulltimeJob and limitedJob
+        if (this.fulltimeJobCheckbox && this.fulltimeJob !== undefined) {
+            this.queryParameters["fulltimeJob"] = JSON.parse(this.fulltimeJob.toString());
+        } else {
+            delete this.queryParameters["fulltimeJob"];
+        }
+
+        if (this.limitedJobCheckbox && this.limitedJob !== undefined) {
+            this.queryParameters["limitedJob"] = JSON.parse(this.limitedJob.toString());
+        } else {
+            delete this.queryParameters["limitedJob"];
+        }
+
+        // Update queryParameters for skill
+        if (this.listsSkillCheckbox && this.skill != undefined) {
+            if (this.skill.trim() !== "") {
+                this.queryParameters["skill"] = this.skill.trim();
+            } else {
+                delete this.queryParameters["skill"];
+            }
+        } else {
+            delete this.queryParameters["skill"];
+        }
+
+        console.log("Updated queryParameters:", this.queryParameters);
     }
 
     generateQuery(): string {
-    let query = this.rdfDataService.prefixes + `SELECT ?s ?title WHERE {
-        ?s rdf:type edm:JobPosting.
-        ?s edm:title ?title.`;
+        const queryMappings: { [key: string]: string } = {
+            jobName: `FILTER contains(?title, "$value$").`,
+            createdBefore: `?s edm:dateCreated ?created. FILTER (xsd:dateTime("$value$T00:00:00Z") > xsd:dateTime(?created)).`,
+            createdAfter: `?s edm:dateCreated ?created. FILTER (xsd:dateTime(?created) > xsd:dateTime("$value$T00:00:00Z")).`,
+            fulltimeJob: `?s mp:isFulltimeJob "$value$"^^xsd:boolean.`,
+            limitedJob: `?s mp:isLimitedJob "$value$"^^xsd:boolean.`,
+            skill: `?s edm:hasSkill ?skill. ?skill edm:textField "$value$"@de`,
+        };
 
-    // Add statements based on queryParameters
-    if (this.queryParameters['jobName']) {
-        query += `
-        FILTER contains(?title, "${this.queryParameters['jobName']}").`;
-    }
+        let query =
+            this.rdfDataService.prefixes +
+            `SELECT ?s ?title WHERE {
+            ?s rdf:type edm:JobPosting.
+            ?s edm:title ?title.`;
 
-    if(this.queryParameters["createdBefore"]) {
-        query += `
-        ?s edm:dateCreated ?created.
-        FILTER (xsd:dateTime("${this.queryParameters['createdBefore']}T00:00:00Z") > xsd:dateTime(?created)).`;
-    }
+        // Add statements based on queryParameters
+        for (const key in this.queryParameters) {
+            if (this.queryParameters.hasOwnProperty(key) && queryMappings[key]) {
+                const filterString = queryMappings[key].replace("$value$", this.queryParameters[key]);
+                query += filterString;
+            }
+        }
 
-    if(this.queryParameters["createdAfter"]) {
-        query += `
-        ?s edm:dateCreated ?created.
-        FILTER (xsd:dateTime(?created) > xsd:dateTime("${this.queryParameters['createdAfter']}T00:00:00Z")).`;
-    }
-
-    //applicants
-
-    if (this.queryParameters.hasOwnProperty('fulltimeJob')) {
-        query += `
-        ?s mp:isFulltimeJob "${this.queryParameters["fulltimeJob"]}"^^xsd:boolean.`;
-    }
-
-    if (this.queryParameters.hasOwnProperty('limitedJob')) {
-        query += `
-        ?s mp:isLimitedJob "${this.queryParameters["limitedJob"]}"^^xsd:boolean.`;
-    }
-
-    if (this.queryParameters['skill']) {
-        query += `
-        ?s edm:hasSkill ?skill.
-		?skill edm:textField "${this.queryParameters['skill']}"@de`;  // doing this over the textfield, since the data:... value doesn't correlate to the skill name for some reason
-    }
-
-    // Close the query
-    query += `}`;
-    console.log(query)
-
-    return query;
+        return query + "}";
     }
 
     onSearch() {
-    let previewQuery = this.generateQuery()
-    this.rdfDataService.getQueryResults(previewQuery).then(data => {
-        this.previewResults = data.results.bindings.map((item) => {
-            let linkParts = item.s.value.split("/")
-            let link = "https://graphdb.elevait.io/resource?uri=http:%2F%2Fai4bd.com%2Fresource%2Fdata%2F" + linkParts[linkParts.length - 1]
-            return {"title": item.title.value, "link": link};
-        });
-    });
+        let previewQuery = this.generateQuery();
+        console.log(previewQuery);
+        console.log("results go here lol");
+
+        // this.rdfDataService.getQueryResults(previewQuery).then(data => {
+        //     this.previewResults = data.results.bindings.map((item) => {
+        //         let linkParts = item.s.value.split("/")
+        //         let link = "https://graphdb.elevait.io/resource?uri=http:%2F%2Fai4bd.com%2Fresource%2Fdata%2F" + linkParts[linkParts.length - 1]
+        //         return {"title": item.title.value, "link": link};
+        //     });
+        // });
     }
 
     chartTypeSelect(event: any) {
         console.log(event.target.id);
-        this.chartType = event.target.id
-        const cardBody = document.querySelector('.preview-content');
+        this.chartType = event.target.id;
+        const cardBody = document.querySelector(".preview-content");
 
-        d3.select(event.target)
-            .style('border', '3px solid gray');
-        d3.selectAll('.img-button')
+        d3.select(event.target).style("border", "3px solid gray");
+        d3.selectAll(".img-button")
             .filter(function () {
                 return this !== event.target;
             })
-            .style('border', 'none');
+            .style("border", "none");
 
-        const creationDateCheckbox = this.propertyCheckboxes.find(checkbox => checkbox.nativeElement.id === 'creationDateProperty');
+        const creationDateCheckbox = this.propertyCheckboxes.find((checkbox) => checkbox.nativeElement.id === "creationDateProperty");
         if (creationDateCheckbox) {
-            creationDateCheckbox.nativeElement.checked = this.chartType === 'line_chart';
-            creationDateCheckbox.nativeElement.disabled = this.chartType === 'line_chart';
+            creationDateCheckbox.nativeElement.checked = this.chartType === "line_chart";
+            creationDateCheckbox.nativeElement.disabled = this.chartType === "line_chart";
         }
 
         let component;
         switch (this.chartType) {
-            case 'bar_chart':
+            case "bar_chart":
                 component = BarChartPreviewComponent;
                 break;
-            case 'line_chart':
+            case "line_chart":
                 component = LineChartPreviewComponent;
                 break;
             default:
-            // Default content or error handling
-            // TODO
-            break;
+                // Default content or error handling
+                // TODO
+                break;
         }
         // Clear the existing content
-        cardBody.innerHTML = '';
+        cardBody.innerHTML = "";
 
         // Dynamically create the component and attach it to the DOM
         const factory = this.componentFactoryResolver.resolveComponentFactory(component);
         const contentComponentRef = factory.create(this.injector);
-        this.currentPreviewContentRef = contentComponentRef
-        
+        this.currentPreviewContentRef = contentComponentRef;
+
         // Type the component instance as any to avoid TypeScript errors
         const contentComponent: any = contentComponentRef.instance;
-        this.currentPreviewContent = contentComponent
+        this.currentPreviewContent = contentComponent;
 
         contentComponent.queryParameters = this.queryParameters;
         contentComponent.selectProperties = this.selectProperties;
@@ -246,45 +232,43 @@ export class VisGenDialogComponent implements OnInit {
     }
 
     updateProperties() {
-    this.selectProperties = this.propertyCheckboxes
-        .filter(checkbox => checkbox.nativeElement.checked)
-        .map(checkbox => checkbox.nativeElement.name);
+        this.selectProperties = this.propertyCheckboxes.filter((checkbox) => checkbox.nativeElement.checked).map((checkbox) => checkbox.nativeElement.name);
 
-    console.log('Selected Properties:', this.selectProperties);
-    if(this.currentPreviewContent !== undefined){
-        this.currentPreviewContent.selectProperties = this.selectProperties;
-        this.currentPreviewContentRef.changeDetectorRef.detectChanges();
-    }
+        console.log("Selected Properties:", this.selectProperties);
+        if (this.currentPreviewContent !== undefined) {
+            this.currentPreviewContent.selectProperties = this.selectProperties;
+            this.currentPreviewContentRef.changeDetectorRef.detectChanges();
+        }
     }
 
     public forwardToEditor() {
         this.chartService.chartType.next(this.chartType);
         if (this.previewResults.length > 0) {
             switch (this.chartType) {
-                case 'bar_chart':
-                    this.dialogService.openBarChartEditor('create', '', this.queryParameters, this.selectProperties, '');
+                case "bar_chart":
+                    this.dialogService.openBarChartEditor("create", "", this.queryParameters, this.selectProperties, "");
                     break;
-                case 'Stacked Bar Chart':
+                case "Stacked Bar Chart":
                     this.dialogService.openStackedBarChartEditor();
                     break;
-                case 'pie_chart':
-                    this.dialogService.openPieChartEditor('create', '', this.queryParameters, this.selectProperties);
+                case "pie_chart":
+                    this.dialogService.openPieChartEditor("create", "", this.queryParameters, this.selectProperties);
                     break;
-                case 'Doughnut Chart':
+                case "Doughnut Chart":
                     this.dialogService.openDoughnutChartEditor();
                     break;
-                case 'star_plot':
+                case "star_plot":
                     this.dialogService.openStarPlotEditor();
                     break;
-                case 'line_chart':
+                case "line_chart":
                     this.dialogService.openLineChartEditor();
                     break;
                 default:
-                    console.log('Invalid Chart Type');
-                    this.dialogService.openSnackBar('Please choose a Visualization Type', 'close');
+                    console.log("Invalid Chart Type");
+                    this.dialogService.openSnackBar("Please choose a Visualization Type", "close");
             }
         } else {
-            this.dialogService.openSnackBar('No database entries to visualize', 'close');
+            this.dialogService.openSnackBar("No database entries to visualize", "close");
         }
     }
 }
