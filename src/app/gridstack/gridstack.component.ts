eae20ba798fb5c76d48b5a278899f3ba9bea810e
zhzhwei <lares2333@gmail.com>
2024-01-27 19:40:44 +0100

    widget removement between inventory and dashboard

--- src/app/gridstack/gridstack.component.ts
+++ src/app/gridstack/gridstack.component.ts
@@ -37,6 +37,8 @@ export class GridStackComponent implements OnInit {
     public majorInitImage: boolean = true;
     public minorInitImage: boolean = true;
     private minorGridEl: any;
+    private tempTileData: any;
+    private tempTileConfig: any;
 
     private dataSources = new Map();
     private resizeObservers = new Map();
@@ -494,13 +496,19 @@ export class GridStackComponent implements OnInit {
             if (this.resizeObservers.has(serial)) {
                 this.resizeObservers.get(serial).disconnect();
             }
+            var visibilityMapping = JSON.parse(localStorage.getItem(serial + "-config"));
+            this.tempTileData = JSON.parse(localStorage.getItem(serial));
+            this.tempTileConfig = visibilityMapping;
             this.chartService.removePersistence(serial);
             if (serial.includes('bar')) {
-                var dataSource = this.dataSources.get(serial);
+                // var dataSource = this.dataSources.get(serial);
+                var dataSource = this.tempTileData.dataSource;
+                var filteredDataSource = dataSource.filter((_, index) => visibilityMapping[index]);
+                console.log(filteredDataSource);
                 var title = this.chartService.chartAction.value.title;
                 var barColor = this.chartService.chartAction.value.barColor;
                 var contEl = document.getElementById(serial);
-                serial = this.gridService.getMinorTileSerial('Bar Chart', serial);
+                serial = this.gridService.getMinorTileSerial('bar_chart', serial);
                 contEl.setAttribute('id', serial);
                 var barEL = document.getElementById(serial);
                 var svg = d3.select('#' + serial).select('svg')
@@ -510,18 +518,18 @@ export class GridStackComponent implements OnInit {
                 d3.select('#' + serial).select('svg').select('foreignObject.download').remove();
                 d3.select('#' + serial).select('svg').select('foreignObject.heart').remove();
                 d3.select('#' + serial).select('svg').select('foreignObject.trash').remove();
-                this.barChart.addHeart(svg, barEL, serial, title, dataSource, 'rgb(255, 0, 0)', barColor);
+                this.barChart.addHeart(svg, barEL, serial, title, filteredDataSource, 'rgb(255, 0, 0)', barColor);
             }
             if (this.minorGridEl.style.display === 'block') {
                 var resizeObserver = new ResizeObserver(entries => {
-                    // console.log('update', serial, jobName, dataSource, titleCount, 'rgb(255, 0, 0)');
-                    this.barChart.copeChartAction('update', serial, title, dataSource, 'rgb(255, 0, 0)', barColor);
+                    // console.log('update', serial, jobName, filteredDataSource, titleCount, 'rgb(255, 0, 0)', barColor);
+                    this.barChart.copeChartAction('update', serial, title, filteredDataSource, 'rgb(255, 0, 0)', barColor);
                 }); 
                 resizeObserver.observe(contEl);
                 this.resizeObservers.set(serial, resizeObserver);
-                this.dataSources.set(serial, dataSource);
+                // this.dataSources.set(serial, filteredDataSource);
                 this.compactGridstack(this.minorGrid);
-                this.chartService.savePersistence('bar_chart', serial, dataSource, title, undefined, 'rgb(255, 0, 0)', barColor);
+                this.chartService.savePersistence('bar_chart', serial, filteredDataSource, title, undefined, 'rgb(255, 0, 0)', barColor);
             }
             setTimeout(() => {
                 if (this.majorGrid.getGridItems().length === 0) {
@@ -550,7 +558,8 @@ export class GridStackComponent implements OnInit {
                 }
                 this.chartService.removePersistence(serial);
                 if (serial.includes('bar')) {
-                    var dataSource = this.dataSources.get(serial);
+                    // var dataSource = this.dataSources.get(serial);
+                    var dataSource = this.tempTileData.dataSource;
                     var title = this.chartService.chartAction.value.title;
                     var heartColor = this.chartService.chartAction.value.heartColor;
                     var barColor = this.chartService.chartAction.value.barColor;
@@ -563,7 +572,6 @@ export class GridStackComponent implements OnInit {
                     var svg = d3.select('#' + serial).select('svg')
                         .attr('width', barEL.clientWidth)
                         .attr('height', barEL.clientHeight)
-                    // console.log(barEL, serial, title, color);
                     d3.select('#' + serial).select('svg').select('foreignObject.heart').remove();
                     this.barChart.addPencil(svg, barEL, serial, title, heartColor, barColor);
                     this.barChart.addDownload(svg, barEL, title, dataSource, heartColor);
@@ -572,12 +580,13 @@ export class GridStackComponent implements OnInit {
                 }
                 if (contEl) {
                     var resizeObserver = new ResizeObserver(entries => {
-                        // console.log('update', serial, jobName, dataSource, titleCount, 'rgb(0, 0, 0)');
+                        // console.log('update', serial, jobName, dataSource, titleCount, 'rgb(0, 0, 0)', barColor);
                         this.barChart.copeChartAction('update', serial, title, dataSource, 'rgb(0, 0, 0)', barColor);
                     });
                     resizeObserver.observe(contEl);
                     this.resizeObservers.set(serial, resizeObserver);
-                    this.dataSources.set(serial, dataSource);
+                    // this.dataSources.set(serial, dataSource);
+                    localStorage.setItem(serial + "-config", JSON.stringify(this.tempTileConfig));
                     this.chartService.savePersistence('bar_chart', serial, dataSource, title, undefined, 'rgb(0, 0, 0)', barColor);
                 }
             }
